75013358cfd86594005eed144ca2907e4704f101
GeraldineBB <geraldine-bornet@hotmail.fr>
2021-11-10 12:08:40 +0100

    Composant Colors et NbColors, on peut appeler le hook UseSelector pour lire les info du state

--- src/components/Colors/index.js
+++ src/components/Colors/index.js
@@ -1,11 +1,33 @@
 import './styles.scss';
 
-const Colors = () => (
-  <div id="colors">
-    <span style={{ color: '#ff0000' }}>#ff0000</span>
-    -
-    <span style={{ color: '#00ff00' }}>#00ff00</span>
-  </div>
-);
+import { useSelector } from 'react-redux';
+
+const Colors = () => {
+
+  // on pourrait utiliser qu'une seule fois le useSelector :
+  // const {firstColor, lastColor} = useSelector(
+  //   (state) => ({firstColor: state.firstColor, lastColor: state.lastColor}),
+  // );
+  // ... mais pas une bonne idée !! 
+  // pour voir si le state évolue il va comparer en surface donc si l'un des deux change, 
+  // ...on va retourner un nouvel objet donc tout est nouveau ... rendu supp alors que pas besoin 
+  // donc c'est bien de faire un useSelector par propriété du state
+
+  const firstColor = useSelector(
+    (state) => state.firstColor,
+  );
+
+  const lastColor = useSelector(
+    (state) => state.lastColor,
+  );
+
+  return (
+    <div id="colors">
+      <span style={{ color: { firstColor } }}>{firstColor}</span>
+      -
+      <span style={{ color: { lastColor } }}>{lastColor}</span>
+    </div>
+  );
+};
 
 export default Colors;
--- src/components/NbColors/index.js
+++ src/components/NbColors/index.js
@@ -1,7 +1,27 @@
+// Si on a besoin de lire de l'information dans le state,
+// On utilise le hook useSelector
+// Ce hook est fourni par react-redux
+import {useSelector} from 'react-redux'; 
 import './styles.scss';
 
-const NbColors = () => (
-  <div id="nbColors">2 couleur(s) générée(s)</div>
-);
+const NbColors = () => {
+
+  // équivalent à faire un store.getState + suscribe 
+  // si le state évolue, le composant sera re rendu automatiquement !
+  // On passe à useSelector une fonction qui permet de
+  // décrire l'information à extraire du state.
+  // Derrière le rideau, useSelector appelera cette fonction en
+  // lui donnant en argument le state courant
+  // et nous renverra le résultat.
+  // Le fait d'utiliser le hook useSelector abonne le composant aux
+  //  mises à jour du state (équivalent à subscribe)
+  const nbColors =  useSelector(
+    (state) => state.NbColors, 
+  )
+
+  return (
+    <div id="nbColors">{nbColors} couleur(s) générée(s)</div>
+  );
+};
 
 export default NbColors;
